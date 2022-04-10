082a86f2f38ced1fe7c6bdc9b0a814576f701406
GanievAR <ganievar@bnipi.rosneft.ru>
2022-04-10 12:27:26 +0500

    Добавили работу через биндинг для активити

--- app/src/main/java/com/example/mvp_mvvm/ui/MainActivity.kt
+++ app/src/main/java/com/example/mvp_mvvm/ui/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.example.mvp_mvvm
+package com.example.mvp_mvvm.ui
 
+import com.example.mvp_mvvm.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvp_mvvm.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
     }
 }
